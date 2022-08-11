48f4e0f3f0cf1b304ccba1d7f9c606b9effa3c26
Ben Simon <ben@ideas2executables.com>
2022-08-11 12:35:44 -0400

    my new take on auto inserts

--- bs-inserts.el
+++ bs-inserts.el
@@ -1,32 +1,29 @@
 ;;
 ;; Define our auto inserts
 ;;
+;; Inspired by: https://github.com/suzp1984/donkey/blob/master/elisp/auto-insert/my-auto-insert.el
+;;
 
-(setq auto-insert-query nil)
-(setq auto-insert-alist '())
-(define-auto-insert "[.]php"
-  '(\n
-    "<?php" \n
-    "/*" \n
-    " * A PHP file for " _ \n
-    " */" \n))
+(require 'autoinsert)
 
-(define-auto-insert "[.]js"
-  '(\n
-    "/*" \n
-    " * A JS file for " _ \n
-    " */" \n))
+(custom-set-variables
+ '(auto-insert-alist '())
+ '(auto-insert 'other)
+ '(auto-insert-directory "~/.emacs.d/inserts/"))
 
-(define-auto-insert "[.]css"
-  '(\n
-    "/*" \n
-    " * A CSS file for " _ \n
-    " */" \n))
+(defun bs-inserts-reset ()
+  "Make sure we have a sane bs-inserts list to work with"
+  (setq auto-insert-alist '()))
 
-(define-auto-insert "[.]el"
-  '(\n
-    ";;;" \n
-    ";;; elisp to  " _ \n
-    ";;;" \n))
+(defun bs-inserts-yas-expand ()
+  "Replace text in yasnippet template."
+  (yas-expand-snippet (buffer-string) (point-min) (point-max)))
+
+(defun bs-insert (regexp path)
+  "Register an auto insert based on pattern and path"
+  (let ((abs-path (expand-file-name (concat "~/.emacs.d/inserts/" path))))
+    (unless (file-exists-p abs-path)
+      (error "Snippet: %s (%s, %s) doesn't exist" abs-path path regexp))
+    (add-to-list 'auto-insert-alist (cons regexp (vector abs-path 'bs-inserts-yas-expand)))))
 
 (provide 'bs-inserts)
