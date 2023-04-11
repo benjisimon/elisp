65c2da712870e7d56fd364de17d5476aa3b3d8cf
Ben Simon <ben@ideas2executables.com>
2023-04-11 07:08:58 -0400

    Always insert unique fake names when asked

--- fake-names.el
+++ fake-names.el
@@ -2,7 +2,7 @@
 ;; Simple commands for accessing fake names. Useful for testing.
 ;;
 
-(defvar fake-name-data-file "~/.emacs.d/local/data/fake-first-names.txt")
+(defvar fake-name-data-file "~/dt/i2x/elisp/src/main/data/fake-first-names.txt")
 (defvar fake-name-posn-file "~/.emacs.d/.cache/fake-name-posn.txt")
 
 (defun fake-name-current-posn ()
@@ -18,24 +18,45 @@
   (with-temp-file fake-name-posn-file
     (insert (format "%d" posn))))
 
-(defun fake-name-next-name ()
-  "Return the next fake name."
+(defun fake-name-peek-next-name ()
+  "Return the next name, but don't save this position."
   (let ((names-buffer (find-file-noselect fake-name-data-file))
         (bol (fake-name-current-posn)))
     (with-current-buffer names-buffer
       (goto-char bol)
       (let* ((eol (line-end-position))
              (name (buffer-substring bol eol)))
-        (fake-name-save-new-posn (+ 1 eol))
         name))))
+
+(defun fake-name-advance-posn (name)
+  "Advance our fake name position by `name`"
+  (let ((current-posn (fake-name-current-posn)))
+    (fake-name-save-new-posn (+ current-posn 1 (length name)))))
+
+(defun fake-name-next-name ()
+  "Return the next fake name."
+  (let ((continue t)
+        (name nil))
+    (while continue
+      (setq name  (fake-name-peek-next-name))
+      (fake-name-advance-posn name)
+      (setq continue (fake-name-exists-in-current-buffer-p name)))
+    name))
 
 (defun insert-fake-name ()
   "Insert the next name in our current buffer"
   (interactive)
   (insert (fake-name-next-name)))
 
+(defun fake-name-exists-in-current-buffer-p (name)
+  "True the fake name exist in the current buffer?"
+  (let ((regexp (concat "\\(\\W\\|^\\)" name "\\(\\W\\|$\\)")))
+    (save-excursion
+      (goto-char (point-min))
+      (re-search-forward regexp nil t))))
+
 (defun fake-name-reset-names ()
-  "Reset our names so we start at the beginning"
+  "Reset our names to the first possible name."
   (interactive)
   (fake-name-save-new-posn 1))
 
