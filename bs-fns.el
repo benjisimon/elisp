d21e543079cc4ff1c446c3595d4455a824bd3727
Ben Simon <ben@ideas2executables.com>
2024-11-02 22:16:56 -0400

    better ediff docs

--- bs-fns.el
+++ bs-fns.el
@@ -234,13 +234,12 @@ This works on the current region."
     (browse-url url)))
 
 (defun ediff-copy-both-to-C ()
+  "Add missing feature to ediff-merge. Support selecting both regions."
   (interactive)
   (ediff-copy-diff ediff-current-difference nil 'C nil
                    (concat
                     (ediff-get-region-contents ediff-current-difference 'A ediff-control-buffer)
                     (ediff-get-region-contents ediff-current-difference 'B ediff-control-buffer))))
-(defun add-d-to-ediff-mode-map () (define-key ediff-mode-map "d" 'ediff-copy-both-to-C))
-(add-hook 'ediff-keymap-setup-hook 'add-d-to-ediff-mode-map)
 
 
 (defun find-snippet-at-point ()
