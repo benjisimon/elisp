75e85ac765d82627a8b38ea07d6f18ab374a6d0e
Ben Simon <ben@ideas2executables.com>
2025-11-10 05:55:13 -0500

    some basic elfeed sync'ing

--- bs-elfeed.el
+++ bs-elfeed.el
@@ -14,4 +14,21 @@
          (new-tag (completing-read "Tag: " available-tags nil nil)))
     (elfeed-search-untag-all (intern new-tag))))
 
+(defun bs-elfeed-db-sync ()
+  "Sync changes to our databse to a git repo for elfeed for remote access."
+  (elfeed-db-unload)
+  (shell-command "elfeedassist -a sync"))
+
+(defun bs-elfeed ()
+  "Kick off elfeed but peform a sync first"
+  (interactive)
+  (bs-elfeed-db-sync)
+  (elfeed))
+
+(defun bs-elfeed-search-quit ()
+  "Quit elfeed, but also do a sync"
+  (interactive)
+  (bs-elfeed-db-sync)
+  (elfeed-search-quit-window))
+
 (provide 'bs-elfeed)
